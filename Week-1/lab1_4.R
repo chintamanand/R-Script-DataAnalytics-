08d17c83b421d9ed82f19d99064ab838b57b6025
chintam anand <chintamanand56@gmail.com>
2018-02-11 23:26:13 +0530

    joint or disjoint sets

---
+++ Week-1/lab1_4.R
@@ -0,0 +1,38 @@
+n1<-as.integer(readline("Nos you want to enter? "))
+
+a <- vector(mode="numeric", length=n1)
+
+for (i in 1:n1)
+{
+ 
+ a[i]<-as.integer(readline("Enter the number "))
+
+}
+
+
+n2<-as.integer(readline("Nos you want to enter? "))
+
+b <- vector(mode="numeric", length=n2)
+
+for (i in 1:n2)
+{
+ 
+ b[i]<-as.integer(readline("Enter the number "))
+
+}
+
+d<-intersect(a,b)
+
+if(length(d)==0)
+{
+  
+print("The sets are disjoint")
+
+}
+
+if(length(d)!=0)
+{
+ 
+ print("The sets are not disjoint")
+
+}
