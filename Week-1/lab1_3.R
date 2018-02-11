7ed9d5965ea9186e0ebd286f29869aae3b317640
chintam anand <chintamanand56@gmail.com>
2018-02-11 23:24:19 +0530

    find number of occurence in csv file

---
+++ Week-1/lab1_3.R
@@ -0,0 +1,14 @@
+mydata = read.csv("data.csv")  # read csv file 
+
+a <- readline(prompt="Enter word to check the occurences : ")
+
+count<-1
+for(i in mydata)
+{
+
+which(mydata == a)
+  
+}
+
+print(count)
+
