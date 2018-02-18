d02e5f358eb9890fa4647f1299eae0d664026d97
chintam anand <chintamanand56@gmail.com>
2018-02-18 19:20:46 +0530

    Density Based Clustering

---
+++ Week-2/DbScan_1.R
@@ -0,0 +1,17 @@
+install.packages("fpc")
+install.packages("dbscan")
+install.packages("factoextra")
+
+  
+  # Load the data 
+  data("multishapes", package = "factoextra")
+df <- multishapes[, 1:2]
+# Compute DBSCAN using fpc package
+library("fpc")
+set.seed(123)
+db <- fpc::dbscan(df, eps = 0.15, MinPts = 5)
+# Plot DBSCAN results
+library("factoextra")
+fviz_cluster(db, data = df, stand = FALSE,
+             ellipse = FALSE, show.clust.cent = FALSE,
+             geom = "point",palette = "jco", ggtheme = theme_classic())
