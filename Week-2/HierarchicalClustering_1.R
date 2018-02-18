db30c99e4276cc80f0d4fa1531d09728613aaf6f
chintam anand <chintamanand56@gmail.com>
2018-02-18 18:02:35 +0530

    Hierarchical Clustering

---
+++ Week-2/HierarchicalClustering_1.R
@@ -0,0 +1,21 @@
+#hierarchical Clustering
+install.packages("factoextra")
+install.packages("cluster")
+install.packages("magrittr")
+
+library("cluster")
+library("factoextra")
+library("magrittr")
+
+res.hc <- USArrests %>%
+  scale() %>%                    # Scale the data
+  dist(method = "euclidean") %>% # Compute dissimilarity matrix
+  hclust(method = "ward.D2")     # Compute hierachical clustering
+# Visualize using factoextra
+# Cut in 4 groups and color by groups
+fviz_dend(res.hc, k = 4, # Cut in four groups
+          cex = 0.5, # label size
+          k_colors = c("#2E9FDF", "#00AFBB", "#E7B800", "#FC4E07"),
+          color_labels_by_k = TRUE, # color labels by groups
+          rect = TRUE # Add rectangle around groups
+)
