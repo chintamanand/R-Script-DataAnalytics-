03c7624ea642d1721dcc170451faf14d7414e5bf
chintam anand <chintamanand56@gmail.com>
2018-02-22 18:28:25 +0530

    Update lab1_4.R

--- Week-1/lab1_4.R
+++ Week-1/lab1_4.R
@@ -1,3 +1,4 @@
+#takes Input
 n1<-as.integer(readline("Nos you want to enter? "))
 
 a <- vector(mode="numeric", length=n1)
@@ -16,23 +17,17 @@ b <- vector(mode="numeric", length=n2)
 
 for (i in 1:n2)
 {
- 
  b[i]<-as.integer(readline("Enter the number "))
-
 }
 
 d<-intersect(a,b)
 
 if(length(d)==0)
 {
-  
 print("The sets are disjoint")
-
 }
 
 if(length(d)!=0)
 {
- 
  print("The sets are not disjoint")
-
 }
