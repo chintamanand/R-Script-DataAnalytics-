c5790b173f6e3ae19ae36b9e972a03e78f2b2da5
chintam anand <chintamanand56@gmail.com>
2018-02-22 18:27:47 +0530

    Update kmeans.R

--- Week-2/kmeans.R
+++ Week-2/kmeans.R
@@ -1,8 +1,10 @@
+#K-means Clustering
 x <- data.frame(y1=rnorm(100, sd = 0.3),y2=rnorm(100, sd = 0.4))
 ch<- kmeans(x, 2)
 # 2 clusters
 plot(x, col = ch$cluster)
 points(ch$centers, col = 1:2, pch = 8, cex = 2)
+
 #using ggplot
 #ggplot(x, aes(y1,y2, color = ch$cluster)) + geom_point()
   #another example
