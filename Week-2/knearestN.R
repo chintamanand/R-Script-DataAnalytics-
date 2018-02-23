b1e2928fd09431e6d030588e1721d970de8345ab
chintam anand <chintamanand56@gmail.com>
2018-02-23 10:47:21 +0530

    Update knearestN.R

--- Week-2/knearestN.R
+++ Week-2/knearestN.R
@@ -1,8 +1,10 @@
+#data
 x1<- c(7,7,3,1)
 x2<- c(7,4,4,4)
 y <- c(1,1,2,2) #label
 df<- cbind(x1,x2) #train dataset
 testDF<- c(3,7) #test dataset
 pred<- knn(train = df, test = testDF, cl = y, k=3)
-pred #shows the label of test data
+pred 
+#shows the label of test data
 
